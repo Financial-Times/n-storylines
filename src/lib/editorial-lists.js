82fb247034ffad3526537e5e52d4905932793e3f
TheoLeanse <theoleanse@gmail.com>
2016-12-15 14:04:17 +0000

    and even more

--- src/lib/editorial-lists.js
+++ src/lib/editorial-lists.js
@@ -1,5 +1,23 @@
 module.exports = function addEditorialPicks (content, listedArticles) {
 
+	const work = x => {
+
+		const replacements = listedArticles
+			  .filter(article => article.year === x.name || article.month === x.name);
+
+		const newRelevantArticles = x.relevantArticles.slice(0) // clone
+			.sort((a, b) => a.weight - b.weight) // sort by ascending weight
+			.slice(0, replacements.length) // remove non-editorial picks
+			.concat(replacements) // add editorial picks
+			.sort((a, b) => {
+				// re-sort by time
+				return a.year - b.year || a.month - b.month || a.day - b.day;
+			});
+
+		x.relevantArticles = newRelevantArticles;
+
+	};
+
 	work(content);
 
 	content.children.forEach(work);
@@ -10,21 +28,4 @@ module.exports = function addEditorialPicks (content, listedArticles) {
 
 	return content;
 
-	function work (x) {
-		const replacements = listedArticles
-			  .filter(article => article.year === x.name || article.month === x.name);
-
-		const newRelevantArticles = x.relevantArticles
-			.slice(0) // clone
-			.sort((a, b) => a.weight - b.weight) // sort by ascending weight
-			.slice(0, replacements.length) // remove non-editorial picks
-			.concat(replacements) // add editorial picks
-			.sort((a, b) => {
-				// re-sort by time
-				return a.year - b.year || a.month - b.month || a.day - b.day ;
-
-			});
-
-		x.relevantArticles = newRelevantArticles;
-	}
-}
+};
--- test/src/lib/editorial-lists.spec.js
+++ test/src/lib/editorial-lists.spec.js
@@ -4,7 +4,27 @@ const addEditorialPicks = require('../../../src/lib/editorial-lists');
 describe('decorating storyline data with editiorial picks', () => {
 	it('happens', () => {
 		const undecorated = require('./fixtures/storyline-data-no-editors-picks');
-		const decorated = addEditorialPicks(undecorated);
+		const picks = [
+			{
+				year: 2016,
+				month: 0,
+				day: 0,
+				weight: 0
+			},
+			{
+				year: 2016,
+				month: 0,
+				day: 0,
+				weight: 0
+			},
+			{
+				year: 2016,
+				month: 0,
+				day: 0,
+				weight: 0
+			}
+		];
+		const decorated = addEditorialPicks(undecorated, picks);
 		expect(decorated).not.eql(undecorated);
 	});
 });
