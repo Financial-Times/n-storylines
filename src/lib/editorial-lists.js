a52458133e79f02d53e3a39339f8db79a8f5c1de
TheoLeanse <theoleanse@gmail.com>
2016-12-15 10:29:05 +0000

    more WIP

--- src/lib/editorial-lists.js
+++ src/lib/editorial-lists.js
@@ -1,5 +1,30 @@
 module.exports = function addEditorialPicks (content, listedArticles) {
-	// replace the least viewed relevant articles with the editorial picks
-	// but we have stripped out the views at this point
+
+	work(content);
+
+	content.children.forEach(work);
+
+	content.children.forEach(child => {
+		child.children.forEach(work);
+	});
+
 	return content;
+
+	function work (x) {
+		const replacements = listedArticles
+			  .filter(article => article.year === x.name || article.month === x.name);
+
+		const newRelevantArticles = x.relevantArticles
+			.slice(0) // clone
+			.sort((a, b) => a.weight - b.weight) // sort by ascending weight
+			.slice(0, replacements.length) // remove non-editorial picks
+			.concat(replacements) // add editorial picks
+			.sort((a, b) => {
+				// re-sort by time
+				return a.year - b.year || a.month - b.month || a.day - b.day ;
+
+			});
+
+		x.relevantArticles = newRelevantArticles;
+	}
 }
