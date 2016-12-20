bdc6aac03726f3580df04a7c2adc90ade2fa013d
Gabrielle von Koss <gabivk@gmail.com>
2016-12-20 10:48:43 +0000

    mustard styles

--- src/main.js
+++ src/main.js
@@ -1,47 +1,42 @@
 const template = require('../templates/main.html');
 
 function init () {
-	let cutsTheMustard = ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window);
-	if (cutsTheMustard) {
-		const initialData = window && window.FT && window.FT.storylineData;
-		if (!initialData) return;
+	const initialData = window && window.FT && window.FT.storylineData;
+	if (!initialData) return;
 
-		const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment');
-		const backBtns = document.getElementsByClassName('n-storylines__back-btn');
+	const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment');
+	const backBtns = document.getElementsByClassName('n-storylines__back-btn');
 
-		setupInteraction(initialData);
+	setupInteraction(initialData);
 
-		function setupInteraction (data) {
-			backBtns[0].style.display = data === initialData ? 'none' : '';
-			if (!data.children) return;
+	function setupInteraction (data) {
+		backBtns[0].style.display = data === initialData ? 'none' : '';
+		if (!data.children) return;
 
-			for (let i = 0; i < heatmapSegments.length; i++) {
-				if (data.children[i].relevantArticles.length > 0) {
-					heatmapSegments[i].addEventListener('click', () => {
-						renderStoryline(data.children[i]);
-						setupBackBtn(data);
-					});
-				} else {
-					let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name');
-					heatmapSegmentsNames[i].classList.add('no-articles');
-				}
+		for (let i = 0; i < heatmapSegments.length; i++) {
+			if (data.children[i].relevantArticles.length > 0) {
+				heatmapSegments[i].addEventListener('click', () => {
+					renderStoryline(data.children[i]);
+					setupBackBtn(data);
+				});
+			} else {
+				let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name');
+				heatmapSegmentsNames[i].classList.add('no-articles');
 			}
 		}
+	}
 
-		function renderStoryline(data) {
-			const component = document.querySelector('.n-storylines');
-			component.parentNode.innerHTML = template(data);
-			setupInteraction(data);
-		}
+	function renderStoryline(data) {
+		const component = document.querySelector('.n-storylines');
+		component.parentNode.innerHTML = template(data);
+		setupInteraction(data);
+	}
 
-		function setupBackBtn (data) {
-			backBtns[0].addEventListener('click', () => {
-				renderStoryline(data);
-				setupBackBtn(initialData);
-			});
-		}
-	} else {
-		console.log('No Mustard Cut');
+	function setupBackBtn (data) {
+		backBtns[0].addEventListener('click', () => {
+			renderStoryline(data);
+			setupBackBtn(initialData);
+		});
 	}
 }
 
